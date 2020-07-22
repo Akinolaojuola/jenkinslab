5d0f59ce5abc11b75ea245749bd5082ed43dbbb2
Akinolaojuola <47824735+Akinolaojuola@users.noreply.github.com>
2020-07-22 19:29:00 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** Hello World, this is the latest change *****-----");
+		System.out.println("-----***** Hello World *****-----");
 	}
 }
